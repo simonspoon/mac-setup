edb5588969526b160b71a3e970a80533f115b11c
Simon <simon@sjspoon.com>
2025-09-02 00:09:18 -0400

    Added dotfile / stow setup to core setup script

--- core_setup.sh
+++ core_setup.sh
@@ -15,9 +15,14 @@ brew install --cask font-fira-code-nerd-font
 brew install neovim
 brew install starship
 brew install nvm
+brew install gh
 
-# Download setup and dotfiles from github
-
-# Backup zshrc, nvim and starship configs by moving them
+# Backup zshrc, nvim, aerospace and starship configs by moving them
+mv ~/.zshrc ~/.zshrc.ms.bak
+mv ~/.tmux.conf ~/.tmux.conf.ms.bak
+mv ~/.config/nvim ~/.config/nvim.ms.bak
+mv ~/.config/starship.toml ~/.config/starship.toml.ms.bak
+mv ~/.config/aerospace ~/.config/aerospace.ms.bak
 
 # Stow the dotfiles
+cd ~/mac-setup/dotfiles/ && stow . -t ~/
