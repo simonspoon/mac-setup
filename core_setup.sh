d27207dc71e1e614b727bf95a3d6f8c4e14dff2b
Simon <simon@sjspoon.com>
2025-09-01 23:55:13 -0400

    Initial Commit

---
+++ core_setup.sh
@@ -0,0 +1,23 @@
+# Install packages
+brew install zoxide
+brew install ripgrep
+brew install fd
+brew install htop
+brew install jq
+brew install stow
+brew install fzf
+brew install bat
+brew install eza
+brew install lazygit
+brew install --cask nikitabobko/tap/aerospace
+brew install tmux
+brew install --cask font-fira-code-nerd-font
+brew install neovim
+brew install starship
+brew install nvm
+
+# Download setup and dotfiles from github
+
+# Backup zshrc, nvim and starship configs by moving them
+
+# Stow the dotfiles
